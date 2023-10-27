0b074df4656c1d53f52180a49d28c65f2a2ed526
SeasonLeague <facebookups23@gmail.com>
2023-10-27 23:23:08 +0100

    Sep files

--- python/bubble/bubble_sort.py
+++ python/bubble/bubble_sort.py
@@ -6,6 +6,6 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 # You can use this function to sort a list of numbers.
-my_list = [64, 34, 25, 12, 22, 11, 90]
+my_list = [64, 34, 25, 12, 22, 11, 90, 91, 92]
 bubble_sort(my_list)
 print("Sorted list:", my_list)
